bf5ac60aae1b53845afed19de53a659ad713f60d
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-12-05 20:39:29 +0100

    Monitor partialy working
    Print last address working
    Run at lasr address working
    TODO :
     - Print address range (BEXAM mode)
     - Input data mode (STORE mode)
    
    ISSUES :
     - PUTC ('\n') should clear the rest of the line ?
     - Floppy data not being loaded. Could be a issue on firmware or virtual
       hardware...

--- cmonitor.asm
+++ cmonitor.asm
@@ -5,6 +5,27 @@
 ;
 ; ------------------------------------------------------------------------------
 
+; Constants
+
+MODE_EXAM     .equ  0           ; Examine a single byte
+MODE_BEXAM    .equ  1           ; Examine a block
+MODE_STORE    .equ  2           ; Write data
+
+PROMPT        .equ 0x5C         ; \
+ADDR_SEP      .equ ':'
+
+CR            .equ 0x0D         ; Enter key
+LF            .equ 0x0A         ; \n
+DEL           .equ 0x08         ; Delete key
+
+; Ram vars
+MKEYB_ADDR:       .EQU 0x1000 ; (dw) Keyboard base address
+LADDRESS:         .EQU 0x1004 ; (dw) Last address pointed
+MBUFFER_COUNT:    .EQU 0x1008 ; (b) Buffer size
+MBUFFER:          .EQU 0x1009 ; (max 255) Buffer size
+
+; Code
+
 MONITOR_ENTRY:
     LOADW %r0, CURSOR_COL
     ADD %r0, %r0, 0x0100
@@ -19,8 +40,13 @@ MONITOR_ENTRY:
     MOV %r0, 0
     STOREB MBUFFER_COUNT, %r0   ; Set buffer counter to 0
 
-MONITOR_PROMT:
-    MOV %r2, ']'
+    MOV %r0, 0x001400
+    STORE LADDRESS, %r0         ; Set last address to 0x001400
+
+MONITOR_PROMPT:
+    MOV %r2, PROMPT
+    CALL PUTC
+    MOV %r2, LF
     CALL PUTC
 
 MONITOR_GETCHAR:
@@ -37,9 +63,9 @@ MONITOR_GETCHAR_WAITLOOP:       ; Busy loop to wait a key
 
 ; Process input from %r2, putting on the buffer and displaying it
 MONITOR_PROCESS_INPUT:
-    IFEQ %r2, 0x0D              ; Enter key
+    IFEQ %r2, CR                ; Enter key
       JMP MONITOR_PARSE
-    IFEQ %r2, 0x08              ; Delete
+    IFEQ %r2, DEL               ; Delete
       JMP MONITOR_DEL
 
     IFGE %r2, 'a'               ; hex
@@ -67,7 +93,7 @@ MONITOR_PROCESS_INPUT:
     JMP MONITOR_GETCHAR_WAITLOOP  ; Unknow, or not an actual command
 
 MONITOR_PUTBUFFER:
-    LOADB %r0, MBUFFER_COUNT    ; Get buffer index
+    LOADB %r0, MBUFFER_COUNT    ; Get buffer size
     STOREB %r0, MBUFFER, %r2    ; Put it on the buffer
     ADD %r0, %r0, 1
     STOREB MBUFFER_COUNT, %r0   ; Update buffer lenght
@@ -77,7 +103,7 @@ MONITOR_PUTBUFFER:
     JMP MONITOR_GETCHAR
 
 MONITOR_DEL:
-    LOADB %r0, MBUFFER_COUNT    ; Get buffer index
+    LOADB %r0, MBUFFER_COUNT    ; Get buffer size
     IFEQ %r0, 0
       JMP MONITOR_GETCHAR       ; Nothing to delete
 
@@ -90,14 +116,125 @@ MONITOR_DEL:
 
 ; Parsing of the buffer
 MONITOR_PARSE:
-    ; TODO
+    ; Jumps to the next line
+    MOV %r2, LF
+    CALL PUTC
+    ; r7 Value
+    ; r8 Mode
+    ; r9 Buffer index
+    ; r10 Buffer size
+    MOV %r7, 0
+    MOV %r8, MODE_EXAM          ; Examine mode by default
+    MOV %r9, 0                  ; Index at 0
+    LOADB %r10, MBUFFER_COUNT   ; Get buffer size
+
+MONITOR_PARSE_FORLOOP:
+    IFGE %r9, %r10
+      JMP MONITOR_PARSE_END
+
+    LOADB %r0, %r9, MBUFFER     ; Load on %r0, buffer[index]
+    IFEQ %r0, 'R'
+      JMP MONITOR_RUN
+    IFEQ %r0, 'r'
+      JMP MONITOR_RUN
+
+    IFEQ %r0, ' '
+      IFEQ %r8, MODE_EXAM
+        JMP MONITOR_NEW_LADDR
+
+    IFEQ %r0, '.'
+      JMP MONITOR_PARSE_FORNEXT ; TODO
+
+    IFEQ %r0, ':'
+      JMP MONITOR_PARSE_FORNEXT ; TODO
+
+    IFLE %r0, '9'               ; 0 to 9
+      JMP MONITOR_VAL_HDIGIT
+
+    IFLE %r0, 'F'               ; A to F
+      JMP MONITOR_VAL_HULETTER
+    JMP MONITOR_VAL_HLLETTER    ; a to f
+
+MONITOR_PARSE_FORNEXT:
+    ADD %r9, %r9, 1
+    JMP MONITOR_PARSE_FORLOOP
+
+MONITOR_PARSE_END:
+    IFEQ %r8, MODE_EXAM
+      STORE LADDRESS, %r7       ; Updates last address
+
+    ; Print value at last address always
+    LOAD %r0, LADDRESS          ; Get last address
+    CALL PUT_UHEX
+
+    MOV %r2, ADDR_SEP
+    CALL PUTC
+    MOV %r2, ' '
+    CALL PUTC
+
+    LOAD %r1, LADDRESS          ; Get last address
+    LOADB %r0, %r1              ; Get value
+    CALL PUT_UBHEX
 
     ; Jumps to the next line
-    MOV %r2, 0x0A
+    MOV %r2, LF
     CALL PUTC
 
     MOV %r0, 0
     STOREB MBUFFER_COUNT, %r0   ; Cleans the buffer lenght
 
-    JMP MONITOR_PROMT
+    JMP MONITOR_PROMPT
+
+; Execute code pointed at last address
+MONITOR_RUN:
+    LOAD %r0, LADDRESS          ; Get last address
+    CALL %r0
+    JMP MONITOR_PARSE_END
+
+; Changes last address and print it
+MONITOR_NEW_LADDR:
+    STORE LADDRESS, %r7
+
+    MOV %r0, %r7
+    CALL PUT_UHEX             ; Print address
+
+    MOV %r2, ADDR_SEP
+    CALL PUTC
+    MOV %r2, ' '
+    CALL PUTC
+
+    LOADB %r0, %r7            ; Print value
+    CALL PUT_UBHEX
+
+    ; Jumps to the next line
+    MOV %r2, LF
+    CALL PUTC
+
+    MOV %r7, 0                ; Reset temporal
+
+    JMP MONITOR_PARSE_FORNEXT
+
+; Shift to the left value and add (0-9)
+MONITOR_VAL_HDIGIT:
+    LLS %r7, %r7, 4
+    SUB %r0, %r0, '0'
+    AND %r0, %r0, 0x0F        ; Sanization
+    OR %r7, %r7, %r0
+    JMP MONITOR_PARSE_FORNEXT
+
+; Shift to the left value and add (A-F) uppercase
+MONITOR_VAL_HULETTER:
+    LLS %r7, %r7, 4
+    SUB %r0, %r0, 0x37        ; ('A' - 0x0A)
+    AND %r0, %r0, 0x0F        ; Sanization
+    OR %r7, %r7, %r0
+    JMP MONITOR_PARSE_FORNEXT
+
+; Shift to the left value and add (A-F) lowercase
+MONITOR_VAL_HLLETTER:
+    LLS %r7, %r7, 4
+    SUB %r0, %r0, 0x57        ; ('a' - 0x0A)
+    AND %r0, %r0, 0x0F        ; Sanization
+    OR %r7, %r7, %r0
+    JMP MONITOR_PARSE_FORNEXT
 
