1160408a41c108b7c1c61a63e5a12cf03319c64f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-12-18 13:09:20 +0100

    More lowercase and identation fix
    Now can be compiled with vasm oldstyle syntax module

--- aux_functions.asm
+++ aux_functions.asm
@@ -12,17 +12,17 @@
 ;   %r2 Size on Double words (bytes / 4). Unsigned double word
 ;   %r2, %r3 and %flags values not preserved
 DW_MEMSET:
-    MOV %r3, 0
-    LLS %r2, %r2, 2         ; x4 -> %r2 now in bytes
+    mov %r3, 0
+    lls %r2, %r2, 2         ; x4 -> %r2 now in bytes
 
 DW_MEMSET_WHILE_LOOP:       ; while (%r3 < %r2)
-    IFGE %r3, %r2
-      JMP DW_MEMSET_END
+    ifge %r3, %r2
+      jmp DW_MEMSET_END
 
-    STORE %r0, %r3, %r1
-    ADD %r3, %r3, 4
-    JMP DW_MEMSET_WHILE_LOOP
+    store %r0, %r3, %r1
+    add %r3, %r3, 4
+    jmp DW_MEMSET_WHILE_LOOP
 
 DW_MEMSET_END:
-    RET
+    ret
 
